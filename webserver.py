0f3809fe9eb9cd0cc8ed00366420313f2cb3f1e4
Kevin McCormack <harlemsquirrel@gmail.com>
2017-12-11 21:31:59 -0500

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -4,6 +4,33 @@
 from flask import Flask
 app = Flask(__name__)
 
+import sqlite3
+
+DATABASE = 'data/data.sqlite3'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/data')
+def data_route():
+    for temp_row in query_db('select * from temps'):
+    print temp_row['location'], ': ', temp_row['temp_c'], 'degrees C, ', temp_row['temp_f'], 'degrees F'
